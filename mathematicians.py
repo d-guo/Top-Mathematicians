ae8c7b576ddea2f635c60f3744988db78ad1d6e6
foodmix <dguo2000@yahoo.com>
2018-09-19 02:57:07 -0700

    added comments

--- mathematicians.py
+++ mathematicians.py
@@ -7,14 +7,14 @@ from bs4 import BeautifulSoup
 create list of mathematicians' names
 	retrieve html file
 	parse through html file
-	prevent duplicates using set
+	prevent duplicates of names using set
 	return list
 
 find popularity score of each mathematician
 	record those who do not exist
 """
 
-def get_content(url) :
+def get_content(url) : #retrieves html file of given url
 	try :
 		resp = get(url)
 	except :
@@ -27,16 +27,16 @@ def get_content(url) :
 		log_error("url format not supported")
 		return None
 
-def file_is_valid(resp) :
+def file_is_valid(resp) :	#checks file to make sure it is html type
 	content_type = resp.headers['Content-Type'].lower()
 	if content_type.find('html') > -1 :
 		return True
 	return False
 
-def log_error(error) :
-	print(error)
+def log_error(error) : #logs error wip
+	#print(error)
 
-def list_of_names(raw_content) :
+def list_of_names(raw_content) :	#returns list of name of mathematicians without duplicates
 	names = set()
 	html = BeautifulSoup(raw_content, 'html.parser')
 	for li in html.select('li') :
@@ -47,13 +47,13 @@ def list_of_names(raw_content) :
 	names = list(names)
 	return names
 
-def get_pop_num(name) :
+def get_pop_num(name) : #returns the popularity score of a given mathematician (returns -1 if mathematician does not exist or can not be found)
 	url = "https://xtools.wmflabs.org/articleinfo/en.wikipedia.org/{}"
 	raw_html = get_content(url.format(name))
 	if raw_html == None :
 		return -1
 	html = BeautifulSoup(raw_html, 'html.parser')
-	for a in html.select('a') :
+	for a in html.select('a') :	#popularity score determined by number of searches in last six months
 		if a['href'].find('latest-60') > -1 :
 			a = a.text.replace(',', '')
 			try :
